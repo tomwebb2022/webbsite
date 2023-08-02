33da3d0df8713aa6885acaa1b2897bfe1ebf5da6
Tom Webb <tomwebb1992@gmail.com>
2023-08-02 15:20:09 +0100

    added intro.js as well as notes for gitignore

--- webbsite/src/App.js
+++ webbsite/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // adding a comment to commit
 //testing again
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>THIS IS MY WEBSITE, THERE ARE MANY LIKE IT. BUT THIS ONE IS MINE</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/tomwebb10/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
         </a>
       </header>
     </div>
---
+++ webbsite/src/Intro/intro.js
@@ -0,0 +1,5 @@
+/* 
+The introduction I will use my 10 second into and talk about my interest in coding and why I wanted to do it in the first place. 
+
+I want to try and link to my linkedin profile and maybe a few other things.
+*/
